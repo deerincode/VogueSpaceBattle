d3e156af28a9327de65839f1a83ba19f5453277b
Vogue Doesong <nboney@email.sc.edu>
2022-11-02 22:35:00 -0400

    first commit. Psuedocode js outlined

---
+++ main.js
@@ -0,0 +1,34 @@
+//Create A ship object
+
+/*
+Ship -
+    hull hitpoints
+    firepower
+    accuracy
+
+    attack ()
+    retreat()
+
+*/
+
+//Create an alien ship
+
+/*
+Alien Ship -
+    hull hitpoints
+    firepower
+    accuracy
+
+    attack()
+
+*/
+
+// Simulate Battle
+
+/*
+Battle - requires Player and Aliens
+    -While either the Player or Alien Ship still has hitpoints, player attacks alien, then alien attacks player
+    -When an alien is destroyed, check if another alien exists. If not, the player wins
+    -If another alien exists, place them in battle, repeat battle
+
+*/
