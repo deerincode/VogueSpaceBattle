b1d50f151d131f1ca5140257038bb67d9b8eee01
Vogue Doesong <nboney@email.sc.edu>
2022-11-04 09:59:19 -0400

    first push - game mechanics laid out

--- main.js
+++ main.js
@@ -23,7 +23,113 @@ Alien Ship -
 
 */
 
+class Ship {
+    constructor(){
+        this.hull = 1
+        this.firepower = 1
+        this.accuracy = .1
+    }
+
+    attack(otherShip){
+        if(this.hull > 0 && otherShip.hull > 0)
+        otherShip.hull = otherShip.hull - this.firepower
+    }
+
+}
+
+class PlayerShip extends Ship {
+    constructor(){
+       super()
+       this.hull = 20
+       this.firepower = 5
+       this.accuracy = 0.7
+    }
+}
+
+class AlienShip extends Ship {
+    constructor(){
+        super()
+        this.hull = 6
+        this.firepower = 2
+        this.accuracy = .6
+    }
+}
+
+class Game {
+    constructor(){
+        this.humanShip = new PlayerShip()
+        this.enemyShips = []
+    }
+
+    gameSetup(){
+        for(let i = 0; i < 6; i++){
+            this.enemyShips[i] = new AlienShip()
+        }
+    }
+
+    startBattle(){
+        
+        while((this.checkIfPlayerIsAlive()) && (this.checkIfAlienIsAlive())){
+            console.log("The while loop starts")
+            this.humanShip.attack(this.enemyShips[0])
+            console.log(`Alien has ${this.enemyShips[0].hull} left`)
+            this.enemyShips[0].attack(this.humanShip)
+            console.log(`Player has ${this.humanShip.hull} left`)
+        }
+        if(this.checkIfAlienIsAlive() == false){
+            console.log("An alien has been defeated")
+        }else if(this.checkIfPlayerIsAlive() == false){
+            console.log("Player has been defeated...")
+        }
+
+        
+    }
+
+    checkIfAlienIsAlive(){
+        if(this.enemyShips[0].hull > 0){
+            return true
+        }else{
+            return false
+        }
+    }
+
+    checkIfPlayerIsAlive(){
+        if(this.humanShip.hull > 0){
+            return true
+        }else{
+            return false
+        }
+    }
+
+    checkIfPlayerWins(){
+        if(! this.enemyShips[0]){
+            console.log(`Player wins!!`)
+        }
+    }
+
+    sendOutNextAlien(){
+        this.enemyShips.shift()
+    }
+
+
+}
+
+// let player = new PlayerShip()
+// let alien = new AlienShip()
+
+// player.attack(alien)
+// console.log(`Alien has ${alien.hull} left`)
+// alien.attack(player)
+// console.log(`Player has ${player.hull} left`)
+
+let game = new Game()
+
+// console.log(game.humanShip)
+game.gameSetup()
+// console.log(game.enemyShips)
 // Simulate Battle
+game.startBattle()
+
 
 /*
 Battle - requires Player and Aliens
